9ac8c2b2d2f8e281076e490087a0843a6f4dd741
Raphaël Huchet <raphaelht@gmail.com>
2020-12-07 18:09:32 +0100

    favicon

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -10,8 +10,18 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>Midikaos</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Midikaos - MIDI Files Library</title>
+        <meta
+          name="description"
+          content="A Standard MIDI Files Library with 3K tracks collected by a music teacher at the end of the 20th century"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-0193GBL6XD"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -27,7 +37,7 @@ const Layout = ({ children }: LayoutProps) => {
       <div className="container mx-auto py-5 text-gray-900">
         <div className="ml-5 flex flex-row">
           <a href="/">
-            <img src="circuit.svg" className="h-8" alt="logo" />
+            <img src="/circuit.svg" className="h-8" alt="logo" />
           </a>
           <h1 className="ml-4 text-3xl font-bold tracking-wider font-mono">
             <a href="/">Midikaos</a>
